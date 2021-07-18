6bbbb97e9647807e9bb51a5543c908ca35d266d6
DengBoCong <1210212670@qq.com>
2021-07-18 23:32:48 +0800

    update

--- src/torch/model/quantiDCE.py
+++ src/torch/model/quantiDCE.py
@@ -1,2 +1,7 @@
-import os
-import json
+from torch import nn
+from transformers import AutoTokenizer, AutoModel
+
+
+class BERTMetric(nn.Module):
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained()
